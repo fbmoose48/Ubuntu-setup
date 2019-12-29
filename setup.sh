5153840065dc8709673591793080324a04d08c6a
fbmoose48 <46300818+fbmoose48@users.noreply.github.com>
2019-12-29 00:19:56 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,45 +8,38 @@ dpkg -l | grep -qw gdebi || sudo apt-get install -yyq gdebi
 
 sudo apt update
 
-sudo apt install virtualbox virtualbox-guest-additions-iso virtualbox-ext-pack \
-net-tools htop lame git mc flatpak audacity \
+sudo apt install gparted cmake net-tools htop lame git mc flatpak audacity \
 openssh-server sshfs gedit-plugin-text-size simplescreenrecorder nano \
-ubuntu-restricted-extras mpv vlc gthumb gnome-tweaks \
-gnome-tweak-tool qt5-style-plugins spell synaptic -yy
+ubuntu-restricted-extras flashplugin-installer mpv vlc gthumb \
+gnome-tweaks gnome-tweak-tool dconf-editor \
+qt5-style-plugins spell synaptic apt-xapian-index \
+chrome-gnome-shell chromium-browser tor expressvpn -yy
+
+# Add PPAs
+sudo add-apt-repository ppa:yannubuntu/boot-repair
+sudo add-apt-repository ppa:danielrichter2007/grub-customizer
+sudo apt-get update
+sudo apt-get install -yy boot-repair grub-customizer
 
 # Add me to any groups I might need to be a part of:
 
-sudo adduser $USER vboxusers
+#sudo adduser $USER vboxusers
 
 # Remove undesirable packages:
 
-sudo apt purge gstreamer1.0-fluendo-mp3 deja-dup shotwell whoopsie whoopsie-preferences -yy
+sudo apt purge gstreamer1.0-fluendo-mp3 deja-dup shotwell popularity-contest whoopsie whoopsie-preferences -yy
 
 # Remove snaps and get packages from apt:
 
-sudo snap remove gnome-characters gnome-calculator gnome-system-monitor
-sudo apt install gnome-characters gnome-calculator gnome-system-monitor \
+sudo snap remove gnome-characters gnome-calculator gnome-system-monitor gnome-logs gnome-system-monitor gtk-common-themes libreoffice
+sudo apt install gnome-characters gnome-calculator gnome-system-monitor gnome-logs gnome-system-monitor gtk-common-themes libreoffice \
 gnome-software-plugin-flatpak -yy
 
-# Purge Firefox, install Chromium:
-
-sudo apt purge firefox -yy
-sudo apt purge firefox-locale-en -yy
-if [ -d "/home/$USER/.mozilla" ]; then
-    rm -rf /home/$USER/.mozilla
-fi
-if [ -d "/home/$USER/.cache/mozilla" ]; then
-    rm -rf /home/$USER/.cache/mozilla
-fi
-
-sudo apt install chromium-browser
-
-gsettings set com.ubuntu.update-notifier show-livepatch-status-icon false
 #set icons to minimize on click
 gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
 
 ## Remove junk
-sudo apt-get remove ubuntu-web-launchers thunderbird rhythmbox -y
+sudo apt-get remove ubuntu-web-launchers rhythmbox -y
 
 ## Multimedia
 sudo apt-get install -y gimp scribus
@@ -54,10 +47,22 @@ sudo apt-get install -y gimp scribus
 ## Games
 sudo apt-get install -y steam-installer
 
+## Turn off Ubuntu-report
+
+ubuntu-report -f send no
+
+## Disable GeoClue/GeoIP service
+
+gsettings set com.ubuntu.geoip geoip-url ""
+
 ## Disable Apport
 sudo sed -i 's/enabled=1/enabled=0/g' /etc/default/apport
 
-# Gotta reboot now:
+## Turn off Live Update Icon
+
+gsettings set com.ubuntu.update-notifier show-livepatch-status-icon false
+
+# Reboot Notification:
 sudo apt update && sudo apt upgrade -y
 
 echo $'\n'$"*** All done! Please reboot now. ***"

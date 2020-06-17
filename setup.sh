ace0fe76d3f11cb6a07e833223458fef48dc2dcf
fbmoose48 <46300818+fbmoose48@users.noreply.github.com>
2020-06-17 18:18:51 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,23 +7,19 @@ dpkg -l | grep -qw gdebi || sudo apt-get install -yyq gdebi
 sudo apt update
 
 # Base system
-sudo apt install ubuntu-restricted-extras openssh-server sshfs openvpn nmap git net-tools nano htop spell neofetch -yy
+sudo apt install ubuntu-restricted-extras gparted gnome-tweaks gnome-tweak-tool dconf-editor synaptic apt-xapian-index solaar solaar-gnome3 nano htop spell neofetch -y
 
+# Web
+sudo apt install openssh-server sshfs openvpn nmap git net-tools openjdk-11-jre flashplugin-installer transmission tor chrome-gnome-shell -y
 
-sudo apt install cmake gettext build-essential hwloc openssl numactl msr-tools  -yy
+# Build tools
+sudo apt install cmake gettext build-essential hwloc openssl numactl msr-tools  -y
 
+# Media
+#sudo apt install lame mpv vlc gthumb audacity soundconverter nextcloud-desktop gimp scribus -y
 
-sudo apt install tor openjdk-11-jre flashplugin-installer transmission nextcloud-desktop -yy
-
-
-sudo apt install lame mpv vlc gthumb audacity soundconverter  -yy
-
-
-sudo apt install gparted gnome-tweaks gnome-tweak-tool dconf-editor chrome-gnome-shell synaptic apt-xapian-index solaar solaar-gnome3  -yy
-
-
-
-flatpak qt5-style-plugins furiusisomount timeshift chromium-browser virtualbox virtualbox-guest-additions-iso virtualbox-ext-pack 
+# Other
+#sudo apt install flatpak qt5-style-plugins furiusisomount timeshift chromium-browser virtualbox virtualbox-guest-additions-iso virtualbox-ext-pack -y
 
 # Add and Install PPAs
 sudo add-apt-repository ppa:yannubuntu/boot-repair
@@ -35,10 +31,11 @@ sudo apt-get install -y boot-repair grub-customizer mkusb mkusb-nox unetbootin
 
 
 # Add me to any groups I might need to be a part of:
-sudo usermod -a -G vboxusers,video $USER
+sudo usermod -a -G video $USER
+#sudo usermod -a -G vboxusers $USER
 
 # Remove undesirable packages:
-sudo apt purge gstreamer1.0-fluendo-mp3 deja-dup shotwell popularity-contest whoopsie whoopsie-preferences -yy
+sudo apt purge gstreamer1.0-fluendo-mp3 deja-dup shotwell popularity-contest -y
 
 # Remove Assorted Arabic/Asian language font types
 sudo apt purge ttf-arabeyes ttf-arphic-uming ttf-indic-fonts-core ttf-kochi-gothic ttf-kochi-mincho ttf-lao ttf-malayalam-fonts ttf-thai-tlwg ttf-unfonts-core -yy
@@ -50,12 +47,13 @@ sudo apt purge ppp pppconfig pppoeconf -yy
 sudo apt purge gnome-games gnome-games-data gnome-cards-data aisleriot gnome-mahjongg gnome-mines gnome-sudoku -yy
 
 # Remove snaps and get packages from apt:
-sudo snap remove gnome-characters gnome-calculator gnome-system-monitor gnome-logs gnome-system-monitor gtk-common-themes libreoffice
-sudo apt install gnome-characters gnome-calculator gnome-system-monitor gnome-logs gnome-system-monitor gtk-common-themes libreoffice \
-gnome-software-plugin-flatpak -yy
+sudo snap remove gnome-characters gnome-calculator gnome-system-monitor gnome-logs gnome-system-monitor gtk-common-themes libreoffice -yy
+sudo apt install gnome-characters gnome-calculator gnome-system-monitor gnome-logs gnome-system-monitor gtk-common-themes libreoffice -yy
 
 #Add flathub repo
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+#Sudo apt install gnome-software-plugin-flatpak -yy
+#flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
 
 #set icons to minimize on click
 gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
@@ -64,13 +62,10 @@ gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
 gsettings set org.gnome.shell.extensions.dash-to-dock show-apps-at-top 'on'
 
 ## Remove junk
-sudo apt-get remove ubuntu-web-launchers rhythmbox transmission-common transmission-gtk -y
+sudo apt-get remove ubuntu-web-launchers rhythmbox -y
 
 # Clean Apt-Cache
 sudo apt-get autoremove && sudo apt-get clean all && sudo apt-get autoclean all
-
-## Multimedia
-# sudo apt-get install -y gimp scribus
 
 ## Games
 # sudo apt-get install -y steam-installer

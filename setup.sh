128b515e70c074ec7b8a7b8b7b45f6552112d9f3
fbmoose48 <46300818+fbmoose48@users.noreply.github.com>
2020-06-17 18:22:03 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -18,8 +18,12 @@ sudo apt install cmake gettext build-essential hwloc openssl numactl msr-tools  
 # Media
 #sudo apt install lame mpv vlc gthumb audacity soundconverter nextcloud-desktop gimp scribus -y
 
+# Virtualization
+#sudo apt install virtualbox virtualbox-guest-additions-iso virtualbox-ext-pack -y
+#sudo usermod -a -G vboxusers $USER
+
 # Other
-#sudo apt install flatpak qt5-style-plugins furiusisomount timeshift chromium-browser virtualbox virtualbox-guest-additions-iso virtualbox-ext-pack -y
+#sudo apt install flatpak gnome-software-plugin-flatpak qt5-style-plugins furiusisomount timeshift chromium-browser -y
 
 # Add and Install PPAs
 sudo add-apt-repository ppa:yannubuntu/boot-repair
@@ -32,28 +36,25 @@ sudo apt-get install -y boot-repair grub-customizer mkusb mkusb-nox unetbootin
 
 # Add me to any groups I might need to be a part of:
 sudo usermod -a -G video $USER
-#sudo usermod -a -G vboxusers $USER
 
 # Remove undesirable packages:
 sudo apt purge gstreamer1.0-fluendo-mp3 deja-dup shotwell popularity-contest -y
 
 # Remove Assorted Arabic/Asian language font types
-sudo apt purge ttf-arabeyes ttf-arphic-uming ttf-indic-fonts-core ttf-kochi-gothic ttf-kochi-mincho ttf-lao ttf-malayalam-fonts ttf-thai-tlwg ttf-unfonts-core -yy
+sudo apt purge ttf-arabeyes ttf-arphic-uming ttf-indic-fonts-core ttf-kochi-gothic ttf-kochi-mincho ttf-lao ttf-malayalam-fonts ttf-thai-tlwg ttf-unfonts-core -y
 
 #Remove dial up et al
-sudo apt purge ppp pppconfig pppoeconf -yy
+sudo apt purge ppp pppconfig pppoeconf -y
 
 #Remove Gnome games
-sudo apt purge gnome-games gnome-games-data gnome-cards-data aisleriot gnome-mahjongg gnome-mines gnome-sudoku -yy
+sudo apt purge gnome-games gnome-games-data gnome-cards-data aisleriot gnome-mahjongg gnome-mines gnome-sudoku -y
 
 # Remove snaps and get packages from apt:
-sudo snap remove gnome-characters gnome-calculator gnome-system-monitor gnome-logs gnome-system-monitor gtk-common-themes libreoffice -yy
-sudo apt install gnome-characters gnome-calculator gnome-system-monitor gnome-logs gnome-system-monitor gtk-common-themes libreoffice -yy
+sudo snap remove gnome-characters gnome-calculator gnome-system-monitor gnome-logs gnome-system-monitor gtk-common-themes libreoffice -y
+sudo apt install gnome-characters gnome-calculator gnome-system-monitor gnome-logs gnome-system-monitor gtk-common-themes libreoffice -y
 
 #Add flathub repo
-#Sudo apt install gnome-software-plugin-flatpak -yy
 #flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-
 
 #set icons to minimize on click
 gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'

cca961ba1fd262b6a0a982ff38199d5dc3b64fdd
fbmoose48 <46300818+fbmoose48@users.noreply.github.com>
2021-02-22 06:07:02 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,7 +11,8 @@ sudo apt install xserver-xorg-core libxinerama-dev libfontconfig1-dev libharfbuz
 sudo apt install xinit --no-install-recommends --no-install-suggests -y
 
 # Base system
-sudo apt install ubuntu-restricted-extras git gparted terminator lxterminal spacefm-gtk3 unzip xarchiver nano htop spell neofetch powertop lm-sensors nnn -y
+sudo apt install ubuntu-restricted-extras git gparted lxterminal spacefm-gtk3 unzip xarchiver nano htop spell neofetch powertop lm-sensors nnn -y
+sudo apt install terminator --no-install-recommends --no-install-suggests -y
 
 # DWM
 sudo apt install fonts-hack fonts-powerline compton lxrandr nitrogen stalonetray -y

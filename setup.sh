15efc59c170a03b57952b52f873e78a8d631e01b
fbmoose48 <46300818+fbmoose48@users.noreply.github.com>
2021-03-08 18:30:48 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,7 +11,8 @@ sudo apt install xserver-xorg-core libxinerama-dev libfontconfig1-dev libharfbuz
 sudo apt install xinit --no-install-recommends --no-install-suggests -y
 
 # Base system
-sudo apt install ubuntu-restricted-extras yaru-theme-icon gparted lxterminal spacefm-gtk3 unzip xarchiver nano nnn -y
+#sudo apt install ubuntu-restricted-extras -y
+sudo apt install yaru-theme-icon gparted lxterminal spacefm-gtk3 unzip xarchiver nano nnn -y
 sudo apt install git htop spell neofetch powertop sysstat lm-sensors -y
 sudo apt install terminator --no-install-recommends --no-install-suggests -y
 
@@ -19,13 +20,13 @@ sudo apt install terminator --no-install-recommends --no-install-suggests -y
 sudo apt install fonts-hack fonts-powerline compton lxrandr nitrogen stalonetray -y
 
 # Greeter
-sudo apt install lightdm lightdm-gtk-greeter --no-install-recommends --no-install-suggests -y
+#sudo apt install lightdm lightdm-gtk-greeter --no-install-recommends --no-install-suggests -y
 
 # Network
 sudo apt install openssh-server sshfs openvpn nmap net-tools -y
 
 # Desktop tools
-sudo apt install synaptic apt-xapian-index conky solaar -y
+#sudo apt install synaptic apt-xapian-index conky solaar -y
 
 # Gnome
 #sudo apt install gnome-tweaks gnome-tweak-tool gnome-icon-theme dconf-editor chrome-gnome-shell solaar-gnome3 -y
@@ -50,28 +51,28 @@ sudo apt install cmake gettext build-essential hwloc openssl numactl msr-tools g
 #sudo apt install flatpak gnome-software-plugin-flatpak qt5-style-plugins furiusisomount timeshift chromium-browser -y
 
 # Add and Install PPAs
-sudo add-apt-repository ppa:yannubuntu/boot-repair
-sudo add-apt-repository ppa:danielrichter2007/grub-customizer
+#sudo add-apt-repository ppa:yannubuntu/boot-repair
+#sudo add-apt-repository ppa:danielrichter2007/grub-customizer
 #sudo add-apt-repository ppa:mkusb/ppa
 #sudo add-apt-repository ppa:gezakovacs/ppa
-sudo apt-get update -y
-sudo apt-get install -y boot-repair grub-customizer
+#sudo apt-get update -y
+#sudo apt-get install -y boot-repair grub-customizer
 #sudo apt-get install -y mkusb mkusb-nox unetbootin
 
 # Add me to any groups I might need to be a part of:
 sudo usermod -a -G video $USER
 
 # Remove undesirable packages:
-sudo apt purge gstreamer1.0-fluendo-mp3 deja-dup shotwell popularity-contest -y
+#sudo apt purge gstreamer1.0-fluendo-mp3 deja-dup shotwell popularity-contest -y
 
 # Remove Assorted Arabic/Asian language font types
-sudo apt purge ttf-arabeyes ttf-arphic-uming ttf-indic-fonts-core ttf-kochi-gothic ttf-kochi-mincho ttf-lao ttf-malayalam-fonts ttf-thai-tlwg ttf-unfonts-core -y
+#sudo apt purge ttf-arabeyes ttf-arphic-uming ttf-indic-fonts-core ttf-kochi-gothic ttf-kochi-mincho ttf-lao ttf-malayalam-fonts ttf-thai-tlwg ttf-unfonts-core -y
 
 #Remove dial up et al
-sudo apt purge ppp pppconfig pppoeconf -y
+#sudo apt purge ppp pppconfig pppoeconf -y
 
 #Remove Gnome games
-sudo apt purge gnome-games gnome-games-data gnome-cards-data aisleriot gnome-mahjongg gnome-mines gnome-sudoku -y
+#sudo apt purge gnome-games gnome-games-data gnome-cards-data aisleriot gnome-mahjongg gnome-mines gnome-sudoku -y
 
 # Remove snaps and get packages from apt:
 #sudo snap remove gnome-characters gnome-calculator gnome-system-monitor gnome-logs gnome-system-monitor gtk-common-themes libreoffice -y
@@ -90,25 +91,25 @@ sudo apt purge gnome-games gnome-games-data gnome-cards-data aisleriot gnome-mah
 #gsettings set org.gnome.shell.extensions.dash-to-dock show-apps-at-top 'on'
 
 ## Remove junk
-sudo apt-get remove ubuntu-web-launchers rhythmbox -y
+#sudo apt-get remove ubuntu-web-launchers rhythmbox -y
 
 # Clean Apt-Cache
 sudo apt-get autoremove && sudo apt-get clean all && sudo apt-get autoclean all
 
 ## Turn off Ubuntu-report
-ubuntu-report -f send no
+#ubuntu-report -f send no
 
 ## Disable GeoClue/GeoIP service
 #gsettings set com.ubuntu.geoip geoip-url ""
 
 ## Disable Apport
-sudo sed -i 's/enabled=1/enabled=0/g' /etc/default/apport
+#sudo sed -i 's/enabled=1/enabled=0/g' /etc/default/apport
 
 ## Turn off Live Update Icon
 #gsettings set com.ubuntu.update-notifier show-livepatch-status-icon false
 
 # Disable GDM Auto-Suspend (Gnome) When no User is logged in
-sudo -u gdm dbus-launch gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
+#sudo -u gdm dbus-launch gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
 
 # Reboot Notification:
 sudo apt update && sudo apt upgrade -y
